d6c018bd2abff2f37fc0f7bcfb72e80ae2a58a34
Pratyush <79155396+Mpratyush54@users.noreply.github.com>
2026-03-28 00:28:06 +0530

    feat: configure server allowed hosts in Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['returns-columns-mart-priorities.trycloudflare.com',
+      'pond-interval-hewlett-consecutive.trycloudflare.com'
+    ],
+  },
 })
